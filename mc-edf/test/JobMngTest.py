216f226a8031052092a73892e6d1841370f7ecac
Jaewoo <jaewoopa@gmail.com>
2015-06-19 21:34:44 +0900

    aaa

--- mc-edf/test/JobMngTest.py
+++ mc-edf/test/JobMngTest.py
@@ -4,18 +4,35 @@ Created on 2015. 6. 19.
 @author: Jaewoo Lee
 https://docs.python.org/2/library/unittest.html
 '''
-
 import util.JobMng as ujob
-ans=[2]
 def test1():
     ujob.clear()
     ujob.insert(1, 3, 4)
     ujob.insert(1, 2, 3)
-    ujob.insert(1, 2, 3)
     return ujob.size()
 
+def test2():
+    ujob.clear()
+    ujob.insert(1, 3, 4)
+    ujob.insert(1, 2, 3)
+    ujob.insert(1, 5, 7)
+    return ujob.size()
+
+fmap={
+      0:test1,
+      1:test2
+}
+ans={
+     0:2,
+     1:3
+}
+
 def main():
-    print test1()    
+    for i in range(len(fmap)):
+        print "test",i
+        if fmap[i]()!=ans[i]:
+            print "error"
+    print "end"    
 if __name__ == '__main__':
     main()
 
---
+++ mc-edf/test/TotalTest.py
@@ -0,0 +1,9 @@
+'''
+Created on 2015. 6. 19.
+
+@author: ÃÖ¿ø¿µ
+'''
+from test import JobMngTest
+
+if __name__ == '__main__':
+    JobMngTest.main()
