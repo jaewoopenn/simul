c1617872629fb15fc3cce060ef021c2073ea6f6a
jaewo <jaewo@asus-jaewoo>
2015-12-12 00:05:24 +0900

    asdas

---
+++ p_graph/src/test/test3.py
@@ -0,0 +1,17 @@
+'''
+Created on 2015. 12. 11.
+
+@author: cpslab
+'''
+import Util.MPlot as mp;
+def main():
+    x=[1,2,3,4]
+    v1=[1,2,3,4]
+    v2=[4,3,2,1]
+    mp.plot(x,v1)
+    mp.plot(x,v2)
+    mp.show()
+    
+
+if __name__ == '__main__':
+    main()
