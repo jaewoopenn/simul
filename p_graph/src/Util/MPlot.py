8f3d17789aef21553f014df88b29ce88a8c2348e
jaewo <jaewo@143.248.163.174>
2015-12-11 23:00:38 +0900

    ghfg

---
+++ p_graph/src/Util/MPlot.py
@@ -0,0 +1,14 @@
+'''
+Created on 2015. 12. 11.
+
+@author: jaewo
+'''
+import matplotlib.pyplot as plt
+import matplotlib
+
+def plot(x,v):
+    plt.plot(x,v)
+    
+def show():
+    plt.ylim(0,5)
+    plt.show()
