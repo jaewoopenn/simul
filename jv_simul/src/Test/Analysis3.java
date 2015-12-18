6cd208f613417e513515dddd33b45be898497c4e
cpslab <cpslab@Jaewoo>
2015-12-18 10:59:14 +0900

    asdda

--- jv_simul/src/Test/Analysis3.java
+++ jv_simul/src/Test/Analysis3.java
@@ -40,59 +40,27 @@ public class Analysis3 {
 
 	public int test3() // exact 1
 	{
-		TaskMng tm=new TaskMng();
-		tm.addTask(2,1);
-		tm.addTask(4,1);
-		tm.addTask(8,1);
-		tm.addTask(16,2);
-		tm.freezeTasks();
-		return Analysis.analEDF(tm);
+		return 1;
 	}
 
 	public  int test4() // MC task
 	{
-		TaskMng tm=new TaskMng();
-		tm.addTask(4,2);
-		tm.addHiTask(6,1,5);
-		tm.freezeTasks();
-		return Analysis.analEDF_VD(tm);
+		return 1;
 	}
 
 	public  int test5()
 	{
-		TaskMng tm=new TaskMng();
-		tm.addTask(13,7);
-		tm.addHiTask(12,4,8);
-		tm.freezeTasks();
-		return Analysis.analEDF_VD(tm);
+		return 1;
 	}
 	
 	public  int test6()
 	{
-		TaskMng tm=new TaskMng();
-		tm.addTask(3,2);
-		tm.addTask(4,1);
-		tm.freezeTasks();
-		int v= Analysis.getRespEDF(tm);
-		Log.prn(1, "res:"+v);
-		return 0;
+		return 1;
 	}
 	
 	public  int test7()
 	{
-		TaskMng tm=new TaskMng();
-		tm.addTask(12,1);
-		tm.addHiTask(3,1,2);
-		tm.addHiTask(4,1,1);
-		tm.freezeTasks();
-		int v;
-		v= Analysis.getRespEDF(tm);
-		Log.prn(1, "EDF res:"+v);
-		v= Analysis.getRespEDF_VD(tm);
-		Log.prn(1, "VD res:"+v);
-		v= Analysis.getRespEDF_TM(tm);
-		Log.prn(1, "TM res:"+v);
-		return 0;
+		return 1;
 	}
 	
 	public  int test8()
