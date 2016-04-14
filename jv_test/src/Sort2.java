8732ddede67e1442e24b9048b655b38188a0590e
cpslab <cpslab@Jaewoo>
2016-04-14 21:55:59 +0900

    asdasda

--- jv_test/src/Sort2.java
+++ jv_test/src/Sort2.java
@@ -12,15 +12,15 @@ public class Sort2 {
 		System.out.println("hihi");
 		Vector<Person> v=new Vector<Person>();
 		v.add(new Person(1,11));
-		v.add(new Person(1,18));
-		v.add(new Person(1,15));
-		v.add(new Person(1,14));
+		v.add(new Person(2,18));
+		v.add(new Person(3,15));
+		v.add(new Person(4,14));
 		for(Person s:v)
 			System.out.println(s.age);
 		System.out.println("---");
 //		v.sort(new PComparator());
-//		Collections.sort(v, new PComparator());
-		Collections.sort(v);
+		Collections.sort(v, new PComparator());
+//		Collections.sort(v);
 //		v.sort(null);
 		for(Person s:v)
 			System.out.println(s.age);
@@ -39,9 +39,9 @@ class Person implements Comparable<Person>
 		return age>o.age?1:0;
 	}
 }
-//class PComparator implements Comparator<Person>{
-//	public int compare(Person arg0, Person arg1) {
-//		System.out.println( arg0.age > arg1.age ?1:0);
-//		return arg0.age > arg1.age ?1:0;
-//	}
-//}	
+class PComparator implements Comparator<Person>{
+	public int compare(Person arg0, Person arg1) {
+		System.out.println( arg0.age > arg1.age ?1:0);
+		return arg0.age > arg1.age ?1:0;
+	}
+}	
