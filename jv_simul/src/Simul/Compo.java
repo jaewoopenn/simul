da428ad6c9acdbb5c0a4c23c74e4ebd9391dc949
jaewo <jaewo@192.249.20.188>
2016-06-07 21:29:45 +0900

    adsad

--- jv_simul/src/Simul/Compo.java
+++ jv_simul/src/Simul/Compo.java
@@ -12,22 +12,34 @@ public class Compo  {
 
 	public void init(TaskMng mng) {
 		tm=mng;
-	}
-	public void compute_X() {
 		lotasks_loutil=tm.getLoUtil();
 		hitasks_loutil=tm.getHiUtil_l();
 		hitasks_hiutil=tm.getHiUtil_h();
+	}
+	public void compute_X() {
 //		glo_x=hitasks_loutil/(1-lotasks_loutil);
 		glo_x=Math.min(1,  (1-hitasks_hiutil)/lotasks_loutil);
 
 		Log.prn(1, "util:"+lotasks_loutil+","+hitasks_loutil+","+hitasks_hiutil);
 		Log.prn(1, "x:"+glo_x);
 	}
-	public void process(){
+	public void help(){
+//		Log.prn(1, "x:"+glo_x);
+		double alpha=0.4;
 		double lo_det=lotasks_loutil+hitasks_loutil/glo_x;
 		double hi_det=glo_x*lotasks_loutil+hitasks_hiutil;
 		Log.prn(1, "lo_det:"+lo_det);
 		Log.prn(1, "hi_det:"+hi_det);
+		double delta=glo_x*hitasks_hiutil-hitasks_loutil
+				-glo_x*(1-glo_x)*lotasks_loutil;
+		Log.prn(1, "delta:"+delta);
+		double re_lo_det=lotasks_loutil+(hitasks_loutil+delta)/glo_x;
+		Log.prn(1, "re_lo_det:"+re_lo_det);
+		double re_hitasks_loutil=hitasks_loutil+alpha*delta;
+		Log.prn(1, "hitasks_loutil:"+hitasks_loutil);
+		Log.prn(1, "re_hitasks_lo_util:"+re_hitasks_loutil
+				+", alpha:"+alpha);
+		Log.prn(1, "strong_hitasks_lo_util:"+(hitasks_loutil+delta));
 	}
 	public boolean isScheduable() {
 		double dtm=lotasks_loutil;
@@ -48,8 +60,11 @@ public class Compo  {
 	}
 
 	
-	public double getX() {
+	public double get_X() {
 		return glo_x;
+	}
+	public void set_X(double d) {
+		glo_x=d;
 	}
 
 
--- jv_simul/src/TestThesis/Sch1.java
+++ jv_simul/src/TestThesis/Sch1.java
@@ -10,7 +10,7 @@ import Simul.TaskMng;
 public class Sch1 {
 	public static int log_level=1;
 //	public static int idx=-1;
-	public static int idx=1;
+	public static int idx=2;
 	public static int total=10;
 	public static int gret[]={0,1,1,1,0,1,0,0,0,0};
 
@@ -22,105 +22,72 @@ public class Sch1 {
 		tm.freezeTasks();
 		return tm;
 	}
-	
+
+	public TaskMng getTask2()
+	{
+		TaskMng tm=new TaskMng();
+		tm.addTask(8,1);
+		tm.addHiTask(12,1,5);
+		tm.freezeTasks();
+		return tm;
+	}
+
 	public int test1() // EDF
 	{
 		TaskMng tm=getTask1();
 		Compo a=new Compo();
 		a.init(tm);
 		a.compute_X();
-		a.process();
 		return -1;
 	}
 
-	public int test2() // EDF-VD det:0.85
+	public int test2() 
 	{
-		TaskMng tm=getTask1();
-		return Analysis.anal_EDF_VD(tm); 
+		TaskMng tm=getTask2();
+		Compo a=new Compo();
+		a.init(tm);
+		a.set_X(0.5);
+		a.help();
+		return -1;
 	}
 
-	public int test3() // EDF-TM det:0.95
+	public int test3() 
 	{
-		TaskMng tm=getTask1();
-		return Analysis.anal_EDF_TM(tm);
+		return -1;
 	}
 	
-	public TaskMng getTask2() // t1 hutil 0.45
+	public  int test4() 
 	{
-		TaskMng tm=new TaskMng();
-		tm.addHiTask(20,2,9);
-		tm.addHiTask(10,2,3);
-		tm.addTask(100,18);
-		tm.addTask(100,12);
-		tm.addTask(100,10);
-		tm.freezeTasks();
-		return tm;
+		return -1;
 	}
 
-	public  int test4() // EDF-VD det:0.95
+	public  int test5()
 	{
-		TaskMng tm=getTask2();
-		return Analysis.anal_EDF_VD(tm); // det:0.855
-	}
-
-	public  int test5() // EDF-TM det:1.05
-	{
-		TaskMng tm=getTask2();
-		return Analysis.anal_EDF_TM(tm);
+		return -1;
 	}
 	
-	public  int test6() // EDF-TM det:0.86
+	public  int test6() 
 	{
-		TaskMng tm=getTask2();
-		return Analysis.anal_EDF_TM_S(tm);
+		return -1;
 	}
 
-	public TaskMng getTask3() // t1 hutil 0.45
+	public  int test7() 
 	{
-		TaskMng tm=new TaskMng();
-		tm.addHiTask(90,5,8);
-		tm.addHiTask(175,1,6);
-		tm.addHiTask(205,14,15);
-		tm.addHiTask(172,2,12);
-		tm.addHiTask(150,7,10);
-		tm.addHiTask(128,2,8);
-		tm.addHiTask(94,1,8);
-		tm.addHiTask(64,1,4);
-		tm.addTask(71,2);
-		tm.freezeTasks();
-		return tm;
-	}
-	public TaskMng getTask4() // t1 hutil 0.45
-	{
-		TaskMng tm=new TaskMng();
-		tm.addHiTask(10,2,3);
-		tm.addHiTask(10,1,5);
-		tm.addTask(10,1);
-		tm.freezeTasks();
-		return tm;
-	}	
-	public  int test7() // EDF sch/ EDF-TM not sch
-	{
-		TaskMng tm=getTask4();
-		tm.prn();
-		int edf=Analysis.anal_EDF(tm);
-		int edf_tm=Analysis.anal_EDF_TM(tm);
-		Log.prn(2, "rs "+edf+" "+edf_tm);
-		return 1;
+		return -1;
 	}
 	
 	public  int test8()
 	{
-		return 0;
+		return -1;
 	}
 	
 	public  int test9()
 	{
-		return 0;
+		return -1;
 	}
 	public  int test10()
 	{
-		return 0;
+		return -1;
 	}
 	
 	@SuppressWarnings("rawtypes")
