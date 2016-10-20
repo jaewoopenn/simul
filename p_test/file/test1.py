b81a25b18961f1068a6a8ff16d0fdefcec554a95
cpslab <cpslab@Jaewoo>
2016-10-20 12:09:48 +0900

    sdfsdf

--- p_test/file/test1.py
+++ p_test/file/test1.py
@@ -3,10 +3,15 @@ Created on 2016. 9. 30.
 
 @author: jaewo
 '''
+import util.FUtil as fu
 def main():
+    test2()
+def test1():
     i_f = open("C:/Users/jaewoo/data/file/test.txt","r")
     for line in i_f:
         val=line.strip()
         print val
+def test2():
+    fu.load("file/test.txt")
 if __name__ == '__main__':
     main()
--- p_test/util/FUtil.py
+++ p_test/util/FUtil.py
@@ -3,3 +3,9 @@ Created on 2016. 10. 19.
 
 @author: jaewo
 '''
+def load(fn):
+    i_f = open("C:/Users/jaewoo/data/file/test.txt","r")
+    for line in i_f:
+        val=line.strip()
+        print val
+    
