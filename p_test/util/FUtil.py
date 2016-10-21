93cd9a37cd9c7e9cb7ef3fcf8bb969533941b7e9
jaewo <jaewo@192.249.20.99>
2016-10-21 17:39:50 +0900

    sad

--- p_test/util/FUtil.py
+++ p_test/util/FUtil.py
@@ -4,13 +4,13 @@ Created on 2016. 10. 19.
 @author: jaewo
 '''
 def loadPrn(fn):
-    i_f = open("C:/Users/jaewoo/data/file/test.txt","r")
+    i_f = open("C:/Users/jaewoo/data/"+fn,"r")
     for line in i_f:
         val=line.strip()
         print val
     
 def load(fn):
-    i_f = open("C:/Users/jaewoo/data/file/test.txt","r")
+    i_f = open("C:/Users/jaewoo/data/"+fn,"r")
     v=[]
     for line in i_f:
         val=line.strip()
