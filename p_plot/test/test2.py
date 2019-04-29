3655d896d207808cf63e7bf03b6ce3a48db5c4c1
JWLEE <JWLEE@JWLee>
2019-04-29 12:13:18 +0900

    adsa

---
+++ p_plot/test/test2.py
@@ -0,0 +1,15 @@
+'''
+Created on 2015. 12. 11.
+
+@author: cpslab
+'''
+import util.MPlot as mp;
+
+if __name__ == '__main__':
+    x=[1,2,3,4]
+    v1=[1,2,3,4]
+    v2=[4,3,2,1]
+    mp.plot(x,v1);
+    mp.plot(x,v2);
+    mp.show()
+    
