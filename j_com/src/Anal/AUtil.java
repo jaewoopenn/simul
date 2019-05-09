b15dd0d014d8163a9203e2172781bb8c157153c9
jaewoo-hp <jaewoopa@gmail.com>
2019-05-09 23:00:46 +0900

    asdasd

--- j_com/src/Anal/AUtil.java
+++ j_com/src/Anal/AUtil.java
@@ -1,6 +1,10 @@
 package Anal;
 
+import com.PRM;
+
 import basic.Task;
+import basic.TaskMng;
+import util.S_Log;
 
 public class AUtil {
 	public static double computeRBF(Task[] ts,int i, int t) {
@@ -15,5 +19,26 @@ public class AUtil {
 		}
 		
 		return r;
+	}
+
+	public static boolean checkSch_ind(PRM p, TaskMng tm, int i, int end_t) {
+		int log_lv=0;
+//		int log_lv=1;
+		S_Log.prn(log_lv, "t \t sup \t req ");
+		for(int t=0;t<end_t;t++) {
+			double s=p.sbf(t);
+			double r=AUtil.computeRBF(tm.getArr(),i,t);
+			String st=t+"\t"+s+"\t"+r+"\t";
+			if (s>r) {
+				st+=">>>>>";
+				S_Log.prn(log_lv,st );
+				return true;
+			} else {
+				st+="<";
+			}
+			S_Log.prn(log_lv,st );
+		}
+		
+		return false;
 	}
 }
--- j_com/src/Anal/z_sch1.java
+++ j_com/src/Anal/z_sch1.java
@@ -1,7 +1,8 @@
-package com;
+package Anal;
 
 
-import Anal.AUtil;
+import com.PRM;
+
 import basic.Task;
 import basic.TaskMng;
 import basic.TaskSet;
@@ -9,39 +10,56 @@ import util.S_Log;
 import util.S_TEngine;
 
 public class z_sch1 {
+	public static int idx=3;
 	public static int log_level=1;
-	public static int idx=1;
-	public static int total=10;
-	public static int gret[]={2,23,0,0,0,0,0,0,0,0};
-	public int test1()
-	{
-		PRM p=new PRM(3,1.5);
+
+	public TaskMng getTM1() {
 		TaskSet ts=new TaskSet();
 		ts.add(new Task(3,1));
 		ts.add(new Task(4,1));
 		ts.end();
-		TaskMng tm=new TaskMng(ts);
+		return new TaskMng(ts);
+	}
+	
+	public int test1()
+	{
+		PRM p=new PRM(3,1.5);
+		TaskMng tm=getTM1();
 		
 		S_Log.prn(1, "t \t sup \t req ");
 		for(int t=0;t<12;t++) {
-			S_Log.prnc(1, ""+t);
 			double s=p.sbf(t);
-			S_Log.prnc(1, "\t"+s);
 			double r=AUtil.computeRBF(tm.getArr(),2,t);
-			S_Log.prnc(1, "\t"+r);
+			String st=t+"\t"+s+"\t"+r+"\t";
 			if (s>r)
-				S_Log.prn(1,"\t >");
+				st+=">>>>>";
 			else
-				S_Log.prn(1,"\t <");
+				st+="<";
+			S_Log.prn(1,st );
 		}
 		return 0;
 	}
 	public int test2() 
 	{
+		PRM p=new PRM(3,1.5);
+//		PRM p=new PRM(3,1);
+		TaskMng tm=getTM1();
+		if(AUtil.checkSch_ind(p,tm,2,13))
+			S_Log.prn(1, "OK");
+		else
+			S_Log.prn(1, "Not OK");
+			
 		return 0;
 	}
 	public  int test3()
 	{
+		PRM p=new PRM(3,1.5);
+//		PRM p=new PRM(3,1);
+		TaskMng tm=getTM1();
+		if(AUtil.checkSch_ind(p,tm,2,13))
+			S_Log.prn(1, "OK");
+		else
+			S_Log.prn(1, "Not OK");
 		return 0;
 	}
 	public  int test4()
@@ -83,5 +101,8 @@ public class z_sch1 {
 		else
 			S_TEngine.runOnce(m,c,aret,idx,log_level);
 	}
+	
+	public static int total=10;
+	public static int gret[]={0,0,0,0,0,0,0,0,0,0};
 
 }
