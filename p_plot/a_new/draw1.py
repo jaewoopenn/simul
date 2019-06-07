7c2ef6509d24a22e16b163c4d5cc90b8e7799389
JWLEE <JWLEE@JWLee>
2019-06-07 10:12:54 +0900

    adsad

--- p_plot/a_new/draw1.py
+++ p_plot/a_new/draw1.py
@@ -15,8 +15,8 @@ class gl:
     vl2=[]
 
 def select():
-    path1()
-#     path2()
+#     path1()
+    path2()
 
 def path1():
     lst=mf.load(gl_inp.fn)
@@ -33,16 +33,31 @@ def path1():
 
 def load(): 
     lst=mf.load(gl_inp.fn)
-        
+    lst2=[]
+    temp=[]
     for m in lst:
-        wd=m.split()
-        gl.xl.append(float(wd[0]))
-        gl.vl.append(float(wd[1]))
+        if m=="---":
+            lst2.append(temp)
+            temp=[]
+        else:
+            temp.append(m)
+    
+    for l in lst2:
+        temp=[]
+        temp2=[]
+        for m in l:
+            wd=m.split()
+            temp.append(float(wd[0]))
+            temp2.append(float(wd[1]))
+        gl.xl.append(temp)
+        gl.vl.append(temp2)
        
 def path2():
     load()
-    mp.plot(gl.xl,gl.vl)  # sbf
-#     mp.plot(gl.xl,gl.vl2)   # rbf
+    for i in range(0,len(gl.xl)):
+#         print(gl.xl[i])
+#         print(gl.vl[i])
+        mp.plot(gl.xl[i],gl.vl[i]) 
     mp.xlim(0,25)
     mp.ylim(0,13)
     mp.ylabel("Resources")
