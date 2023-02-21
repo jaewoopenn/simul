1b0e40c844befdb475d415265291396ae778435b
jaewoo <jaewoo@10.3.8.88>
2023-02-20 22:51:28 -1000

    asdas

--- jv_com2/src/testPlatform/Platform4.java
+++ jv_com2/src/testPlatform/Platform4.java
@@ -41,7 +41,7 @@ public class Platform4 {
 //		p.setStart(100);
 //		p.setSize(1);
 		p.setStep(5);
-		return p; //lll
+		return p; 
 	}
 	public PlatformCom getP2() {
 		PlatformCom p=getCommmon();
