9cb7ce919be96b6eb926745a000442ba6a0141e1
jaewoo <jaewoo@DESKTOP-SCHOOL>
2023-07-31 16:56:17 +0900

    asasdsa

--- p_graph/a_imc/sch_anal.py
+++ p_graph/a_imc/sch_anal.py
@@ -7,15 +7,10 @@ import util.MFile as mf
 import util.MPlot as mp;
 class gl_input:
     fn="run/imc1/a_graph.txt"
-    xlab= "Utilization Bound"
-    ylab= "Acceptance Ratio"
 
 class gl:
-    lab=[]
     x=[]
     vv=[]
-    line=['r--','k-','b-.','g--','m:']
-    marker=['o','v','D','^','s']
 
 def load():
     i_f = mf.load(gl_input.fn)
@@ -24,14 +19,8 @@ def load():
         val=line.strip().split(" ")
         raw.append(val)
     itemlen=len(raw[0])
-#     print(itemlen)
-
-    for i in range(1,itemlen):
-        gl.lab.append(raw[0][i])
     for i in range(1,len(raw)):
         gl.x.append(str(int(raw[i][0])/100))
-#         gl.x.append(raw[i][0])
-#     print(gl.x)
 
     for i in range(1,itemlen):
         v=[]
@@ -45,17 +34,11 @@ def load():
 def main():
     mp.prepare3()
     load()
-    no=0
-    for v in gl.vv:
-        print(v)
-        mp.plot3(gl.x,v,gl.line[no],gl.lab[no],gl.marker[no])
-        no+=1
-#     mp.ylim(0, 1.02)
-    mp.legendBL()
-    mp.xlabel(gl_input.xlab)
-    mp.ylabel(gl_input.ylab)
-    mp.show()
-#     mp.savefig(mf.filepath("run/sch_imc.pdf"))
+    for i in range(11):
+        x=gl.vv[0][i]-gl.vv[1][i]
+        y=gl.vv[0][i]-gl.vv[2][i]
+        z=gl.vv[0][i]-gl.vv[3][i]
+        print(gl.x[i],x,y,z)
 
 if __name__ == '__main__':
     main()
