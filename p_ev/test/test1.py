8078606b6544a4032129877a158770d90d209bdd
jaewoo <jaewoo@DESKTOP-SCHOOL>
2024-05-29 10:32:28 +0900

    asda

--- p_ev/test/test1.py
+++ p_ev/test/test1.py
@@ -3,7 +3,18 @@ Created on 2015. 12. 11.
 
 @author: cpslab
 '''
+class gl:
+    path=1
+def test1():
+    pass
+def test2():
+    pass
+    
 def main():
+    if gl.path==1:
+        test1()
+    elif gl.path==2:
+        test2()
     print("hihi")
 
 if __name__ == '__main__':
