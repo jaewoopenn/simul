91e1d24e8f98c7407ef2357769cb9db192ebba42
jaewoo <jaewoo@DESKTOP-SCHOOL>
2024-08-06 14:24:01 +0900

    asdasda

---
+++ p_ev/graph/sch.py
@@ -0,0 +1,66 @@
+'''
+Draw Acceptance Ratio //// MC-FLEX
+
+@author: cpslab
+'''
+import util.MFile as mf
+import util.MPlot as mp;
+class gl_input:
+    fig="ev/sch.pdf"
+    fn="ev/var/a_graph.txt"
+    xlab= "Utilization bound"
+    ylab= "Acceptance ratio"
+
+class gl:
+    lab=[]
+    x=[]
+    vv=[]
+    line=['k-','r-','b-','g-','m--']
+    marker=['o','v','D','^','s']
+
+def load():
+    i_f = mf.load(gl_input.fn)
+    raw=[]
+    for line in i_f:
+#         val=line.strip().split("\t")
+        val=line.strip().split()
+#         print(val)
+        raw.append(val)
+    itemlen=len(raw[0])
+#     print(itemlen)
+
+    for i in range(1,itemlen):
+        gl.lab.append(raw[0][i])
+    for i in range(1,len(raw)):
+        gl.x.append(str(int(raw[i][0])/100))
+#         gl.x.append(raw[i][0])
+#     print(gl.x)
+
+    for i in range(1,itemlen):
+        v=[]
+        for j in range(1,len(raw)):
+            v.append(float(raw[j][i]))
+#         print(v)
+        gl.vv.append(v)
+        
+        
+
+def main():
+#     mp.prepare()
+    mp.prepare4()
+    load()
+    no=0
+    for v in gl.vv:
+        print(v)
+        mp.plot3(gl.x,v,gl.line[no],gl.lab[no],gl.marker[no])
+        no+=1
+#     mp.ylim(0, 1.02)
+    mp.legendBL()
+    mp.xlabel(gl_input.xlab)
+    mp.ylabel(gl_input.ylab)
+    mp.xlab_rot(13)
+#     mp.show()
+    mp.savefig(mf.filepath(gl_input.fig))
+
+if __name__ == '__main__':
+    main()
---
+++ p_ev/log/test5.py
@@ -0,0 +1,85 @@
+'''
+Created on 2015. 12. 11.
+
+@author: cpslab
+'''
+
+from util.MFile import CFile
+from log.MLog import CLog
+from log.MConfig import CConfig
+import util.MRand as mr
+
+class gl:
+#     path=1
+#     path=2
+    path=3
+#     path=4
+
+
+
+def test1():
+    for i in range(1,10):
+        c=CConfig()
+        c.init_w("ev/var/config"+str(i)+".txt")
+        c.addKV("num","10")
+        c.addKV("prob",format(i*0.1,".2f"))
+        c.addKV("k3","v3")
+        c.end()    
+
+
+def test2():
+    for i in range(1,10):
+        c=CConfig()
+        c.init_r("ev/var/config"+str(i)+".txt")
+        w=c.get('num')
+        print(w)
+        w=c.get('prob')
+        print(w)
+
+def gen(fn):
+    cf=CFile()
+    cf.open_w(fn)
+    t=0
+    end_t=20
+    arr_p=0.7
+    while t<=end_t:
+        if mr.pick()>arr_p:
+            t+=1
+            continue
+        d=mr.pickInt(5, 20)
+        m=mr.pickInt(2,7)
+        o=mr.pickInt(1,4)
+        str="%d %d %d %d"%(t,d,m,o)
+        cf.write(str)
+        t+=1
+    cf.end()
+
+    
+def run(idx):
+    c=CConfig()
+    fn="ev/var/config"+str(idx)+".txt"
+    c.init_r(fn)
+    n=int(c.get('num'))
+    for i in range(n):
+        fn="ev/var/data"+str(idx)+"-"+str(i)+".txt"
+        gen(fn)
+def test3():
+    run(1)
+#     for i in range(1,10):
+#         run("ev/var/config"+str(i)+".txt")
+
+def test4():
+    pass
+    
+def main():
+    if gl.path==1:
+        test1()
+    elif gl.path==2:
+        test2()
+    elif gl.path==3:
+        test3()
+    elif gl.path==4:
+        test4()
+
+if __name__ == '__main__':
+    main()
