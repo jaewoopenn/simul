bbb755bd20cd1c7dd6ee0ccbaafcb774990fdd69
jaewoo <jaewoo@localhost>
2025-04-24 16:21:43 +0100

    adsdas

---
+++ j_adm/src/util/MOut.java
@@ -0,0 +1,32 @@
+package util;
+
+import java.io.File;
+import java.io.PrintWriter;
+/*
+
+
+*/
+
+public class MOut {
+	private PrintWriter g_writer=null;
+	
+	public MOut(String file) {
+		try {
+			g_writer = new PrintWriter(MDir.path+file);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+	}
+	public void write(String txt){
+		g_writer.println(txt);
+	}
+	public void save()
+	{
+		g_writer.close();
+	}
+	
+
+}
