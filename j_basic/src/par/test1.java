43df34030d20639b26492257e1d6cda54b837542
jaewoo <jaewoo@168-213.noc.soton.ac.uk>
2025-05-16 17:52:39 +0100

    dasda

--- j_basic/src/par/test1.java
+++ j_basic/src/par/test1.java
@@ -1,32 +1,54 @@
 package par;
 
+import java.util.stream.IntStream;
+
+import util.MRand;
+import util.SLog;
+
 class MyRunnable implements Runnable {
+	protected MRand g_rutil=new MRand();
+	public int ret=0;
     @Override
     public void run() {
     	for(int i=0;i<10;i++) {
-	        System.out.println(i+"Thread running: " + Thread.currentThread().getName());
+    		ret=g_rutil.getInt(100);
+    		SLog.prn(1,i+" "+ret+" Thread running: " + Thread.currentThread().getName());
 	        try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
     	}
-	       
     }
+	public int getRet() {
+		return ret;
+	}
 }
 
 public class test1 {
 
 	public static void main(String[] args) throws InterruptedException {
-		Thread t1 = new Thread(new MyRunnable());
-		t1.start(); // Starts a new thread
+		SLog.set_lv(1);
+		int n=100;
+		MyRunnable[] mr=new MyRunnable[n];
+		Thread[] th=new Thread[n];
+		int[] ran=IntStream.range(0,n).toArray();
+		for(int i:ran) {
+			mr[i]=new MyRunnable();
+			th[i] = new Thread(mr[i]);
+			th[i].start(); // Starts a new thread
+			Thread.sleep(1000/n);
+		}
 		
-		Thread.sleep(500);
-		Thread t2 = new Thread(new MyRunnable());
-		t2.start(); // Starts a new thread
-		t1.join();
-		t2.join();
-        System.out.println("join");
+		for(int i:ran) {
+			th[i].join();
+		}
+		
+		int ret=0;
+		for(int i:ran) {
+			ret+=mr[i].getRet();
+		}
+        SLog.prn(1,"join "+ret);
 	 
 	}
 
